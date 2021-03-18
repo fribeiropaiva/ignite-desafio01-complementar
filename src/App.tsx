004eb296ace13cac756ba65ca6d70cb8fce0977f
ribeiro240 <filipepaiva240@gmail.com>
2021-03-17 22:23:05 -0300

    Create SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -59,22 +59,7 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              id={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-      </nav>
+      <SideBar genres={genres} selectedGenreId={selectedGenreId} handleClickButton={handleClickButton} />
       <Content selectedGenre={selectedGenre} movies={movies} />
 
     </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    title: string;
+    name: string;
+  }>;
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SideBarProps) {
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
